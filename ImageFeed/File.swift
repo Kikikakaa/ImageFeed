b21a5f988a82fb7cb8e0128faec68dbc49efc249
Никита <uzer591@gmail.com>
2025-03-24 01:02:42 +0500

    launch screen, main screen added

---
+++ ImageFeed/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by user on 09.03.2025.
+//
+
+import Foundation
---
+++ ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by user on 09.03.2025.
+//
+
+import UIKit
+final class ImagesListCell: UITableViewCell {
+    
+}
+
