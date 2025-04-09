2563f7143d1c433d9ac7df5b3b96eae71fe5c44f
Никита <uzer591@gmail.com>
2025-04-09 21:49:19 +0500

    ProfileViewController made with code, scrollView, share button

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -1,12 +1,73 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private weak var avatarImageView: UIImageView!
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var loginNameLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    @IBOutlet private weak var logoutButton: UIButton!
     
-    @IBAction func didTapLogoutButton() {
+    private let profileImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let tagLabel = UILabel()
+    private let descriptionLabel = UILabel()
+    private let logoutButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        profileImageView.image = UIImage(named: "Avatar")
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+        
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        nameLabel.textColor = UIColor(hex: "FFFFFF")
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        tagLabel.text = "@ekaterina_nov"
+        tagLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        tagLabel.textColor = UIColor(hex: "AEAFB4")
+        tagLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tagLabel)
+        
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        descriptionLabel.textColor = UIColor(hex: "FFFFFF")
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        logoutButton.setImage(UIImage(named: "logout_button"), for: .normal)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            
+            tagLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            tagLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: tagLabel.bottomAnchor, constant: 8),
+            
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    @objc private func didTapLogoutButton() {
+        
+        print("Logout button tapped")
     }
 }
+
---
+++ UIColors+Extension/UIColors+Extension.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        self.init(
+            red: CGFloat(r) / 255,
+            green: CGFloat(g) / 255,
+            blue: CGFloat(b) / 255,
+            alpha: alpha
+        )
+    }
+}
